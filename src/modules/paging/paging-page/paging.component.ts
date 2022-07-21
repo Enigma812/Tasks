66932074d9681174a7b5805c5d9f0a91a0acec04
Enigma812 <antiseptic812@gmail.com>
2022-07-21 11:01:26 +0300

    feat: observalble in progress

--- src/modules/paging/paging-page/paging.component.ts
+++ src/modules/paging/paging-page/paging.component.ts
@@ -11,6 +11,9 @@ export class PagingPageComponent {
 
   public items$: Observable<string[]>;
   public buttons$: Observable<number[]>;
+  public active: boolean = false;
+  public total$: Observable<number>;
+  public act$: Observable<number>
 
   constructor(
     private readonly _pagingService: PagingService
@@ -18,6 +21,7 @@ export class PagingPageComponent {
     this.items$ = this._pagingService.state$.pipe(
       map((state) => state.items)
     )
+
     this.buttons$ = this._pagingService.state$.pipe(
       map((state) => {
         const pagesCount = Math.ceil(state.total / state.pageSize);
@@ -28,9 +32,27 @@ export class PagingPageComponent {
         return arrButtons;
       })
     )
+
+    this.total$ = this._pagingService.state$.pipe(
+      map((state) => state.total)
+    )
+
+    this.act$ = this._pagingService.state$.pipe(
+      map((state) => state.pageNumber)
+    )
   }
 
   public changePage(pageNumber: number): void {
     this._pagingService.changePage(pageNumber);
   }
+
+  // public activeButton(page: Observable<number>): boolean {
+  //     if(page = this.act$) {}
+  //     return this.active = true;
+  //   }
+  
+
+    public activeButton(): void {
+      this.active = true;
+    }
 }
